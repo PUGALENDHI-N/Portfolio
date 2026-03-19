879fe8666158ee0242a5a56e5b92f05b7dd6a688
PUGALENDHI N <pugalendhinagaraj29@gmail.com>
2026-03-19 14:31:34 +0530

    fix router import error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Routes, Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./component/Navbar";
 import Home from "./pages/Home";
@@ -10,18 +10,16 @@ import Footer from "./pages/Footer";
 function App() {
   return (
     <>
-    <BrowserRouter>
-     <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        
-        <Route path="/project" element={<Project />} />
-      </Routes>
-  
-    </BrowserRouter>
-   <Footer/>
-   </>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/project" element={<Project />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </>
   );
 }
 
